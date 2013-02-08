c72243b88ce1aa1ecb123e88b09ee6c5471f4caa
John Meehan <john.meehan1@gmail.com>
2013-02-08 01:27:27 +0000

    Cleaned up the routes and romoved static links - all rspec tests pass

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,15 +9,15 @@ describe "Static Pages" do
   describe "Home page" do
     it "it should have the h1 'Sample App'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('h1', :text => 'Sample App')
     end
    it "should have the base title" do
-   		visit '/static_pages/home'
+   		visit root_path
    		page.should have_selector('title', :text => "Sample App")
    end
    it "should not have a custom page title" do
-   	visit '/static_pages/home'
+   	visit root_path
    	page.should_not have_selector('title',:text => '| Home')
    end
   end
@@ -25,24 +25,24 @@ describe "Static Pages" do
    describe "Help page" do
     it "it should have the h1 'Help'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('h1', :text => 'Help')
    	end
 
    	it "should have the title 'Help'" do
-   		visit '/static_pages/help'
+   		visit help_path
    		page.should have_selector('title', :text => "#{base_title} | Help")
    	end
    end
 
   describe "About page" do
   	it "should have the h1 'About Us'" do
-  		visit '/static_pages/about'
+  		visit about_path
   		page.should have_selector('h1', :text => 'About Us')
 
   	end
   	it "should have the title 'About Us'" do
-  		visit '/static_pages/about'
+  		visit about_path
   		page.should have_selector('title', :text => "#{base_title} | About Us")
   	end
   end
@@ -50,12 +50,13 @@ describe "Static Pages" do
    describe "Contact Page" do
     it "it should have the h1 'Contact'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('h1', :text => 'Contact')
    	end
+   	
 
    	it "should have the title 'Contact'" do
-   		visit '/static_pages/contact'
+   		visit contact_path
    		page.should have_selector('title', :text => "#{base_title} | Contact")
    	end
    end
