be2779c4e981638fa7760206faf01d01575a1f06
John Meehan <john.meehan1@gmail.com>
2013-02-12 23:36:37 +0000

    Handled a fails sign in

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+	def new
+
+	end
+
+	def create
+		user = User.find_by_email(params[:session][:email])
+		if user && user.authenticate(params[:session][:password])
+			#sign the user in and redirect to the user's show page
+		else
+			#flash.now to fix flash persistance error
+			flash.now[:error] = 'Invalid email/password combination'
+			render 'new'
+		end
+	end
+
+	def destroy
+
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 SampleApp::Application.routes.draw do
   resources :users
+  #ch8 no need to show or edit sessions to just creating these
+  resources :sessions, only:[:new, :create, :destroy]
+  match '/signup', to: 'users#new'
+  match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   root to: 'static_pages#home'
   #get "users/new" # removed in 7.1
